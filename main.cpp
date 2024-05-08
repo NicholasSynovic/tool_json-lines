079b5aed444290afbc4eda38d6c384b467dfb47d
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-05-08 15:05:56 -0500

    Update example files from the template

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include "CLI/CLI.hpp"
+#include <iostream>
 
-int main() {
-    printf("Hello World!\n");
+using namespace std;
+
+int main(int argc, char **argv) {
+    CLI::App app{"Hello World Example"};
+
+    string text = "Hello World!";
+    app.add_option("-i", text, "Text to print")    ->capture_default_str();
+
+    CLI11_PARSE(app, argc, argv);
+
+    cout << text << "\n";
     return 0;
 }
