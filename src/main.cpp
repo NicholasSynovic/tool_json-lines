563dc60bc5e2dc21610bea5032b4dc63e5e00cf9
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-05-10 10:58:24 -0500

    Remove OpenMP pragma

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <filesystem>
 #include "nlohmann/json.hpp"
 #include <indicators/progress_bar.hpp>
+#include <indicators/indeterminate_progress_bar.hpp>
+#include "main.hpp"
 
 using namespace indicators;
 using namespace std;
@@ -15,10 +17,22 @@ vector<string> readFile(path file)  {
 
     vector<string> data = {};
 
+    IndeterminateProgressBar ibar{
+        option::BarWidth{MAX_BAR_SIZE},
+        option::Start{"["},
+        option::Fill{"."},
+        option::Lead{"<=>"},
+        option::End{"]"},
+        option::PostfixText{string("Reading ").append(file.filename())},
+        option::ForegroundColor{Color::green},
+        option::FontStyles{std::vector<FontStyle>{FontStyle::bold}},
+    };
+
     jsonFile.open(file);
     if(jsonFile.is_open())  {
         while(getline(jsonFile, line))  {
             data.insert(data.end(), line);
+            ibar.tick();
         };
         jsonFile.close();
     };
@@ -30,30 +44,28 @@ vector<json> convertToJson(vector<string> jsonStrings)   {
     int jsonStringsSize = jsonStrings.size();
 
     ProgressBar bar{
-        option::BarWidth{10},
+        option::BarWidth{MAX_BAR_SIZE},
         option::Start{"["},
         option::Fill{"="},
         option::Lead{">"},
         option::Remainder{" "},
         option::End{"]"},
-        option::PostfixText{"Converting string to JSON"},
+        option::PostfixText{"Converting strings to JSON"},
         option::ForegroundColor{Color::green},
         option::FontStyles{std::vector<FontStyle>{FontStyle::bold}},
         option::MaxProgress{jsonStringsSize},
+        option::ShowElapsedTime{true},
+        option::ShowRemainingTime{true},
     };
 
     vector<json> data(jsonStringsSize);
 
     int counter = 0;
-    #pragma omp parallel for
+    /* #pragma omp parallel for */
     for(int i = 0; i < jsonStringsSize; i++) {
         json jsonData = json::parse(jsonStrings[i]);
         data[i] = jsonData;
-
-        #pragma omp critical
-        {
-            bar.tick();
-        }
+        bar.tick();
     }
 
     return data;
---
+++ src/main.hpp
@@ -0,0 +1,11 @@
+#ifndef MAIN
+#define MAIN
+
+// Functions declarations
+void readFile();
+void convertStringToJSON();
+
+// Global Variables
+int MAX_BAR_SIZE = 20;
+
+#endif
