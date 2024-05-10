e748e83df401644748ee7a92392baae92bb6d330
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-05-10 11:17:48 -0500

    Add CPP and header files for JSON handling

---
+++ src/json/json.cpp
@@ -0,0 +1,37 @@
+#include "../common/common.hpp"
+#include <indicators/progress_bar.hpp>
+#include "json.hpp"
+
+using namespace std;
+using namespace indicators;
+
+vector<json> convertStringsToJSON(vector<string> jsonStrings)   {
+    int jsonStringsSize = jsonStrings.size();
+
+    ProgressBar bar{
+        option::BarWidth{MAX_BAR_SIZE},
+        option::Start{"["},
+        option::Fill{"="},
+        option::Lead{">"},
+        option::Remainder{" "},
+        option::End{"]"},
+        option::PostfixText{"Converting strings to JSON"},
+        option::ForegroundColor{Color::green},
+        option::FontStyles{std::vector<FontStyle>{FontStyle::bold}},
+        option::MaxProgress{jsonStringsSize},
+        option::ShowElapsedTime{true},
+        option::ShowRemainingTime{true},
+    };
+
+    vector<json> data(jsonStringsSize);
+
+    int counter = 0;
+    /* #pragma omp parallel for */
+    for(int i = 0; i < jsonStringsSize; i++) {
+        json jsonData = json::parse(jsonStrings[i]);
+        data[i] = jsonData;
+        bar.tick();
+    }
+
+    return data;
+}
---
+++ src/json/json.hpp
@@ -0,0 +1,9 @@
+#include "nlohmann/json.hpp"
+
+#ifndef MAIN
+#define MAIN
+
+// Functions declarations
+std::vector<nlohmann::json> convertStringToJSON(std::vector<std::string> jsonStrings);
+
+#endif
